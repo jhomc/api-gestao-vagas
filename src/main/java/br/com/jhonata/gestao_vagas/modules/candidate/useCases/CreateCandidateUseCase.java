c14e3ca9995a3ebfe295fe5351a3774e0ace1e73
Colegiosuicoti <ti@chpr.com.br>
2025-05-29 16:48:25 -0300

    encoder for candidate

--- src/main/java/br/com/jhonata/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/jhonata/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.jhonata.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.jhonata.gestao_vagas.exceptions.UserFoundException;
@@ -13,12 +14,19 @@ public class CreateCandidateUseCase {
   @Autowired
   private CandidateRepository candidateRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public CandidateEntity execute(CandidateEntity candidateEntity) {
     this.candidateRepository
         .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
         .ifPresent((user) -> {
           throw new UserFoundException("Já existe um usuário com este username ou email");
         });
+
+    var password = passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
+
     return this.candidateRepository.save(candidateEntity);
   }
 }
