437122bb2aebab13bdb753ca5b8c6ff21e417c2f
Colegiosuicoti <ti@chpr.com.br>
2025-06-10 15:27:24 -0300

    swagger install

--- src/main/java/br/com/jhonata/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/jhonata/gestao_vagas/GestaoVagasApplication.java
@@ -3,11 +3,15 @@ package br.com.jhonata.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", description = "API responsável pela gestão de vagas", version = "1"))
 public class GestaoVagasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestaoVagasApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GestaoVagasApplication.class, args);
+  }
 
 }
