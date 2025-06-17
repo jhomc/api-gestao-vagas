7afbac871cc7d7f38340b40cb285eb3ead4061fa
Colegiosuicoti <ti@chpr.com.br>
2025-06-17 16:47:34 -0300

    swagger auth

--- src/main/java/br/com/jhonata/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/jhonata/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.jhonata.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jhonata.gestao_vagas.exceptions.UserFoundException;
 import br.com.jhonata.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.jhonata.gestao_vagas.modules.candidate.useCases.CandidateProfileUseCase;
 import br.com.jhonata.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.jhonata.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
+import br.com.jhonata.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -27,6 +39,9 @@ public class CandidateController {
 
   @Autowired
   private CandidateProfileUseCase candidateProfileUseCase;
+
+  @Autowired
+  private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -49,5 +64,19 @@ public class CandidateController {
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
+  }
+
+  @GetMapping("/job")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  @Tag(name = "Candidato", description = "Informações do candidato")
+  @Operation(summary = "Lista vagas de acordo com o filtro", description = "Essa função é responsável por listar as vagas de acordo com o filtro")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", content = {
+          @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+      }),
+  })
+  @SecurityRequirement(name = "jwt_auth")
+  public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+    return this.listAllJobsByFilterUseCase.execute(filter);
   }
 }
